e1f7855ede54afebf740a6a1d471164a874dc1b1
mennnndy <mennnndy@levi.com>
2024-09-12 10:59:20 +0300

    finsh the Test

--- exam.js
+++ exam.js
@@ -4,10 +4,7 @@ const Mission1 = (x) => {
   return x.filter((x) => x % 2 === 0);
 };
 
-//Displaying a printout to test the function individually for myself
-console.log(Mission1([12, 112, 4, 2, 1, 3, 5, 6]));
-
-//
+//Mission2
 //A function that returns the number of words containing four letters
 const Mission2 = (x) => {
   let result = [];
@@ -19,24 +16,23 @@ const Mission2 = (x) => {
   return result.length;
 };
 
-//Displaying a printout to test the function individually for myself
-console.log(Mission2(["Mission2", "Levi", "Mosa", "Meni", "Mike"]));
-
 //Mission3
 //A function that accepts a two-dimensional array and turns it into a one-dimensional array
 const Mission3 = (x) => {
-    return x.reduce(function(a, b) {
-        return a.concat(b);
-    });;
+  return x.reduce(function (a, b) {
+    return a.concat(b);
+  });
 };
-let x = [[0, 0, 1],[4, 4, 5],]
-
-//Displaying a printout to test the function individually for myself
-console.log(Mission3(x));
-
-
+let x = [
+  [0, 0, 1],
+  [4, 4, 5],
+];
 
 
-
-
+module.exports = {
+    Mission1,
+    Mission2,
+    Mission3
+}
+//Without 4 and 5 and 6
 
