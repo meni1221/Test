96045c1012bbde73084f9ae4f0b681bf378b65c0
mennnndy <mennnndy@levi.com>
2024-09-12 09:46:25 +0300

    Mission1 Filtering the array and displaying only even numbers and returning a new array

---
+++ exam.js
@@ -0,0 +1,7 @@
+// Filtering the array and displaying only even numbers and returning a new array
+const Mission1 = (x) => {
+  return x.filter((x) => x % 2 === 0);
+};
+
+//Displaying a printout to test the function individually for myself
+console.log(Mission1([12, 112, 4, 2, 1, 3, 5, 6]));
